c70f61db489946136035f8ead39df1902a51af50
Laura <185547145+garalau@users.noreply.github.com>
2024-10-30 14:10:56 +0100

    Corrección redireccion dasboard.
    Si volvíamos a la página de inicio nos salía la página de
    users aunque fueramos admin.
    
    para solucionarlo en la página web.php y welcome.blade
    y navigation.blade buscamos donde pone route('dashboard')
    y lo modificamos por route('home').
    
    En web.php comentamos
    /* Route::get('/dashboard', function () {
        return view('dashboard');
    })->middleware(['auth', 'verified'])->name('dashboard'); */
    
    y añadimos ->name(home) al route creado
    Route::get('/home',[HomeController::class, 'index'])->name('home');

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
 
-Route::get('/home',[HomeController::class, 'index']);
+Route::get('/home',[HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
