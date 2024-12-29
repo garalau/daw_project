241e0f7ac17bf1a9c8d70c654446155c84e36570
MiguelDLRR <miguelrrojo@gmail.com>
2024-12-29 20:29:08 +0100

    comentado de funcion registrosProyectos por fallo con tabla user

--- app/Models/EspeciesConiferas.php
+++ app/Models/EspeciesConiferas.php
@@ -20,11 +20,11 @@ class EspeciesConiferas extends Model
         'b_factor',
     ];
 
-
+/*
     //RELACION CON REGISTROS PROYECTOS. TODAVIA NO FUNCIONA
     public function registrosProyectos()
     {
         return $this->hasMany(RegistrosProyectos::class, 'especie_id');
-    }
+    }*/
 
 }
